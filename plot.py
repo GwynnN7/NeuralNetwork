8cd3a8cb8035cb2a782590d66bfc8f6691466cc3
GwynnN7 <mattcheru03@gmail.com>
2026-07-19 22:38:15 +0200

    Update plot method

--- plot.py
+++ plot.py
@@ -1,17 +1,20 @@
 import pandas as pd
+import sys
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+file_path = f"build/{sys.argv[1]}" if len(sys.argv) > 1 else "build/log.csv"
 fig, ax = plt.subplots(figsize=(8, 5))
 
 def update(frame):
     ax.clear()
     try:
-        df = pd.read_csv("build/loss_log.csv")
-        
-        ax.plot(df['epoch'], df['loss'], linewidth=2, color='blue', label='Loss')
+        df = pd.read_csv(file_path)
+
+        ax.plot(df['epoch'], df['train_loss'], linewidth=2, color='blue', label='Train Loss')
+        ax.plot(df['epoch'], df['test_loss'], linewidth=2, color='red', label='Test Loss')
         ax.set_ylim(bottom=0)
-        ax.set_title("Live Training Loss")
+        ax.set_title("Loss Curves")
         ax.set_xlabel("Epoch")
         ax.set_ylabel("Loss")
         ax.legend()
@@ -19,5 +22,6 @@ def update(frame):
     except Exception as e:
         pass
 
-ani = FuncAnimation(fig, update, interval=50)
-plt.show()
+if __name__ == "__main__":
+    anim = FuncAnimation(fig, update, interval=50)
+    plt.show()
