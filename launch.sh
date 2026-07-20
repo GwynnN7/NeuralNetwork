5d4e815ff2e16b06178789e71c8c99bfe02b776a
GwynnN7 <mattcheru03@gmail.com>
2026-07-20 12:07:01 +0200

    Enable optimizations

--- launch.sh
+++ launch.sh
@@ -2,10 +2,18 @@
 
 BUILD_DIR="build"
 
-cmake --build $BUILD_DIR/
+cmake -B $BUILD_DIR -DCMAKE_BUILD_TYPE=Release
+cmake --build $BUILD_DIR -j 6
 
-./$BUILD_DIR/NeuralNet xor_hot \
-    --epochs 5000 --eta 0.3 --batch 4 \
-    --output linear \
-    --hidden relu \
-    --network 3
+# ./$BUILD_DIR/NeuralNet xor \
+#     --epochs 5000 --eta 0.5 --batch 4 \
+#     --output relu \
+#     --hidden sigmoid \
+#     --network 2
+
+./$BUILD_DIR/NeuralNet mnist \
+    --epochs 1000 --eta 0.3 --batch 1000 --lambda 1e-6 \
+    --dataset_ratio 0.5 \
+    --output sigmoid \
+    --hidden sigmoid \
+    --network 128 64
